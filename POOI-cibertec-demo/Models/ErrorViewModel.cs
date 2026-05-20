6c6eeb7c11485d83977e99a307503da81c7f5e09
bryant-con-t <bry.y.valen@gmail.com>
2026-05-20 13:18:13 -0500

    initial commit

---
+++ POOI-cibertec-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POOI_cibertec_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
